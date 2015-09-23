0bef14484fef57617a32c3e81e7f1fc80f06d435
Adam Garstang <a.garstang@coolblue.nl>
2015-09-23 16:11:43 +0200

    Update to allow use on Windows

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,6 +7,14 @@ class r {
     'RedHat': {
       package {'R-core': ensure => installed}
     }
+    'windows': {
+      # Choco package does not install static version and does not add R to PATH
+      exec { 'Install R Windows':
+        command  => template('r/windows_install_r.ps1.erb'),
+        provider => powershell,
+        unless   => "if(Test-Path \"\${Env:ProgramFiles}\\R\\R-*\\bin\\R.exe\"){exit 0}else{exit 1}",
+      }
+    }
     default: { fail("Not supported on osfamily ${::osfamily}") }
   }
 
--- manifests/package.pp
+++ manifests/package.pp
@@ -1,14 +1,52 @@
-define r::package($r_path = '/usr/bin/R', $repo = 'http://cran.rstudio.com', $dependencies = false) {
+define r::package($r_path = '', $repo = 'http://cran.rstudio.com', $dependencies = false) {
 
-  $command = $dependencies ? {
-    true    => "${r_path} -e \"install.packages('${name}', repos='${repo}', dependencies = TRUE)\"",
-    default => "${r_path} -e \"install.packages('${name}', repos='${repo}', dependencies = FALSE)\""
-  }
+    case $::osfamily {
+    '^(Debian|RedHat)$': {
 
-  exec { "install_r_package_${name}":
-    command => $command,
-    unless  => "${r_path} -q -e '\"${name}\" %in% installed.packages()' | grep 'TRUE'",
-    require => Class['r']
+      if $r_path == '' {
+        $binary = '/usr/bin/R'
+      }
+      else
+      {
+        $binary = $r_path
+      }
+
+      $command = $dependencies ? {
+        true    => "${binary} -e \"install.packages('${name}', repos='${repo}', dependencies = TRUE)\"",
+        default => "${binary} -e \"install.packages('${name}', repos='${repo}', dependencies = FALSE)\""
+      }
+
+      exec { "install_r_package_${name}":
+        command => $command,
+        unless  => "${binary} -q -e \"'${name}' %in% installed.packages()\" | grep 'TRUE'",
+        require => Class['r']
+      }
+
+    }
+    'windows': {
+
+      if $r_path == '' {
+        $binary = 'r.exe'
+      }
+      else
+      {
+        $binary = $r_path
+      }
+
+      $deps = $dependencies ? {
+        true    => 'TRUE',
+        default => 'FALSE'
+      }
+
+      exec { "install_r_package_${name}":
+        command  => template('r/windows_install_rpackage.ps1.erb'),
+        provider => powershell,
+        unless   => template('r/windows_rpackage_check.ps1.erb'),
+        require  => Class['r']
+      }
+
+    }
+    default: { fail("Not supported on osfamily ${::osfamily}") }
   }
 
 }
